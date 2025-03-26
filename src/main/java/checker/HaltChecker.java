4d91b3b27a720c2b0fc03ba860a3daeedf58a2ee
KeyKoder <victor.fernandez.montano@gmail.com>
2025-03-26 15:33:53 +0100

    Implementar un prototipo de la funcionalidad de HaltChecker

--- src/main/java/checker/HaltChecker.java
+++ src/main/java/checker/HaltChecker.java
@@ -5,8 +5,26 @@ import program.HaltingProgram;
 import program.Program;
 
 public class HaltChecker {
-	public boolean checkHalt(Program program) {
-		// TODO: Ask for advice on how to actually implement this
-		return program instanceof HaltingProgram;
+	public static final int EXECUTE_TIME_LIMIT_MILLIS = 20000; // 20 sec
+	public boolean checkHalt(Program program, Object... programInput) {
+		Thread programRunner = new Thread() {
+			public void run() {
+				System.out.println("ret " + program.getClass().getName() + " -> " + program.execute(programInput));
+			}
+		};
+
+		programRunner.start();
+
+		try {
+			programRunner.join(EXECUTE_TIME_LIMIT_MILLIS);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println("alive " + program.getClass().getName() + " -> " + programRunner.isAlive());
+		if (programRunner.isAlive()) {
+			programRunner.interrupt();
+			return false;
+		}
+		return true;
 	}
 }
