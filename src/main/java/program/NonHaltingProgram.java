48aeb2acba2b3b674f7915768062b70f5bd24777
KeyKoder <victor.fernandez.montano@gmail.com>
2025-03-26 14:47:16 +0100

    Modificar programa para que sean más genericos

--- src/main/java/program/NonHaltingProgram.java
+++ src/main/java/program/NonHaltingProgram.java
@@ -3,8 +3,9 @@ package program;
 public class NonHaltingProgram implements Program {
 
 	@Override
-	public void execute(int input) {
-		for(int i = 0; i < input; i--) {
+	public void execute(Object input) {
+		Integer i = 0;
+		while(i != input) {
 			System.out.println(i);
 		}
 	}
