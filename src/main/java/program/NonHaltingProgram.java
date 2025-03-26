c312cbdf4f41207cf94a04df24d990597e5830c2
KeyKoder <victor.fernandez.montano@gmail.com>
2025-03-26 14:44:25 +0100

    Hacer que los bucles de los programas hagan algo

--- src/main/java/program/NonHaltingProgram.java
+++ src/main/java/program/NonHaltingProgram.java
@@ -5,7 +5,7 @@ public class NonHaltingProgram implements Program {
 	@Override
 	public void execute(int input) {
 		for(int i = 0; i < input; i--) {
-			// Do things
+			System.out.println(i);
 		}
 	}
 }
