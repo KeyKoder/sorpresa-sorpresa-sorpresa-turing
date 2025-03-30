b74402a2d53eeb6b06029223dd4c3764c98a39dc
KeyKoder <victor.fernandez.montano@gmail.com>
2025-03-30 18:59:20 +0200

    Comentar código y modificar los prints del programa para que sean más expresivos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,21 +9,21 @@ public class Main {
 	public static void main(String[] args) {
 		HaltChecker haltChecker = new HaltChecker();
 		ProgramFactory hpf = new HaltingProgramFactory();
-		System.out.println("HaltingProgram");
+		System.out.println("Halting Program");
 		System.out.println("Halted: " + haltChecker.checkHalt(hpf.createProgram(), 100));
 		System.out.println();
 
 		ProgramFactory nhpf = new NonHaltingProgramFactory();
-		System.out.println("NonHaltingProgram");
+		System.out.println("Non Halting Program");
 		System.out.println("Halted: " + haltChecker.checkHalt(nhpf.createProgram(), 100));
 		System.out.println();
 
 		Program rev = new Reverser();
-		System.out.println("Reverser H");
+		System.out.println("Reverser Halting");
 		System.out.println("Halted: " + haltChecker.checkHalt(rev, hpf, 100));
 		System.out.println();
 
-		System.out.println("Reverser NH");
+		System.out.println("Reverser Non Halting");
 		System.out.println("Halted: " + haltChecker.checkHalt(rev, nhpf, 100));
 		System.out.println();
 	}
--- src/main/java/checker/HaltChecker.java
+++ src/main/java/checker/HaltChecker.java
@@ -5,11 +5,14 @@ import program.HaltingProgram;
 import program.Program;
 
 public class HaltChecker {
+	// Como no es posible hacer una maquina/código que adivine si un programa termina o no
+	// Vamos a simularlo haciendo que espere un tiempo determinado, y si el programa no ha terminado
+	// tras ese tiempo, se considerará que el programa no termina
 	public static final int EXECUTE_TIME_LIMIT_MILLIS = 20000; // 20 sec
 	public boolean checkHalt(Program program, Object... programInput) {
 		Thread programRunner = new Thread() {
 			public void run() {
-				System.out.println("ret " + program.getClass().getName() + " -> " + program.execute(programInput));
+				System.out.println("Program " + program.getClass().getName() + " returned! Value: " + program.execute(programInput));
 			}
 		};
 
@@ -20,7 +23,7 @@ public class HaltChecker {
 		} catch (InterruptedException e) {
 			throw new RuntimeException(e);
 		}
-		System.out.println("alive " + program.getClass().getName() + " -> " + programRunner.isAlive());
+		System.out.println("Is program " + program.getClass().getName() + " still alive? -> " + programRunner.isAlive());
 		if (programRunner.isAlive()) {
 			programRunner.interrupt();
 			return false;
