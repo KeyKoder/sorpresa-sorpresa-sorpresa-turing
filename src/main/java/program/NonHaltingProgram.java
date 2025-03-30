5f4565b9e73272d08e5850be9a0ab2aef2856b15
KeyKoder <victor.fernandez.montano@gmail.com>
2025-03-30 18:59:20 +0200

    Comentar código y modificar los prints del programa para que sean más expresivos

--- src/main/java/program/NonHaltingProgram.java
+++ src/main/java/program/NonHaltingProgram.java
@@ -7,7 +7,10 @@ public class NonHaltingProgram implements Program {
 	public Object execute(Object... input) {
 		Integer i = 0;
 		while(i != input[0]) {
-//			System.out.println(i);
+			// No hacer nada, o contar hacia abajo, sería lo mismo por ahora
+			// Exceptuando el caso de que si contamos hacia abajo, eventualmente el programa terminaría
+			// por que llegaría al tamaño minimo de un entero en java (-2147483648 o Integer.MIN_VALUE) y haría overflow, contando desde el
+			// tamaño máximo de un entero (2147483647 o Integer.MAX_VALUE) hacia abajo hasta llegar al número deseado
 		}
 		return i;
 	}
