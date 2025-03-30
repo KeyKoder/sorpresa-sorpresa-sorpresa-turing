ce596712e12b903556e98332dce12465f9eaf9fd
KeyKoder <victor.fernandez.montano@gmail.com>
2025-03-30 19:35:14 +0200

    Eliminar archivos del builder

--- src/main/java/MaquinaTurim/Builder/ProgramBuilder.java
+++
@@ -1,8 +0,0 @@
-package MaquinaTurim.Builder;
-
-interface ProgramBuilder {
-    void setName(String name);
-    void setCode(String code);
-    void setTerminates(boolean terminates);
-    Program getResult();
-}
